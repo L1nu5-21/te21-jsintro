b125971e120eeddf6ac147c0bad49931123d4035
Linus Lundqvist <linus.lundqvist2@elev.ga.ntig.se>
2023-08-31 11:01:30 +0200

    ✨ neat new stuff

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 console.log("Bonjour World")
+
+let firstname
+firstname = "yo mama"
+
+let lastname = "on pizza"
+
+let num = 0
+
+console.log(typeof(num))
+
+
+const NAME = "Bob"
+console.log(NAME)
+
+let i
+
+for ( i = 0; i < 10; i++) {
+    const NAME = "Baloni"
+    console.log(i)
+    console.log(NAME)
+}
+
+console.log(NAME)
+
+console.log(i)
+
+var skola = "NTI"
+console.log(skola)
+
+
+let a = 'Frjens' // String
+let b = 'L bozo' // String
+let c = 'Mitt namn är ${a}' // String med template literals
+let d = 'bobua grahm is here, say hi ' + b // kontaktenering
+console.log(c)
+
+let e = 12 // num
+let f = 16.7 // num
+
+let g = true // boolean
+let h = false // boolean
+
+
+console.log
